9112e6387fc6bbf4ce0fcc25c73938f25aab29c9
JenCastro <56638573+jscastro-asu@users.noreply.github.com>
2021-12-29 00:29:04 -0800

    Hangman project different set of challenges working towards diagram without using functions.

---
+++ Day7/Day7-repl-finalhangman.py
@@ -0,0 +1,57 @@
+
+#This is a Hangman Game
+
+import random
+import hangman_words # from hangman_words import word_list
+import hangman_art # from hangman_art import stages,logo
+
+(exec(open('hangman_words.py').read()))
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+(exec(open('hangman_art.py').read()))
+print(logo)
+
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+      print("You already used this letter.")
+      continue
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+       
+    if guess not in chosen_word:
+        
+        lives -= 1
+        print(f"{guess} is not in the word.")
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    
+    (exec(open('hangman_art.py').read()))
+    
+    print(stages[lives])
