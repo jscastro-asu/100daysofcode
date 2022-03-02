36e18b7491bc2e6cdf52744eeb679d19c90cd16d
JenCastro <56638573+jscastro-asu@users.noreply.github.com>
2022-03-01 23:00:56 -0800

    Using OOP for a Quiz Game.

---
+++ Day18 - Intermediate/data.py
@@ -0,0 +1,73 @@
+question_data = [
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Cold War ended with Joseph Stalin&#039;s death.",
+        "correct_answer": "False"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Tiananmen Square protests of 1989 were held in Hong Kong.",
+        "correct_answer": "False"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States Department of Homeland Security was formed in response to the September 11th attacks.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In World War ll, Great Britian used inflatable tanks on the ports of Great Britain to divert Hitler away from Normandy\/D-day landing.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Adolf Hitler was tried at the Nuremberg trials.",
+        "correct_answer": "False"
+
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Kublai Khan is the grandchild of Genghis Khan?",
+        "correct_answer": "True"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The French Kingdom helped the United States gain their independence over Great Britain during the Revolutionary War.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States of America was the first country to launch a man into space.",
+        "correct_answer": "False"
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Thomas Crapper was a plumber who invented the flushing toilet.",
+        "correct_answer": "False"
+    }
+]
