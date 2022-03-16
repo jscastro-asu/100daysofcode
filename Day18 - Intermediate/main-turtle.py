6806a2b0beb65e635973a1cab3d2c0bf6fd5f374
JenCastro <56638573+jscastro-asu@users.noreply.github.com>
2022-03-15 20:42:11 -0700

    Adding more completed activities.

---
+++ Day18 - Intermediate/main-turtle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+from tkinter import *
+
+donatello = Turtle()
+print(donatello.shape("heart"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
