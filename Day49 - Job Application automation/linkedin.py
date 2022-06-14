3b751dd3031bb308eb60cea004e169c2f6543382
JenCastro <56638573+jscastro-asu@users.noreply.github.com>
2022-06-13 19:26:44 -0700

    Selenium - Twitter automation

--- Day49 - Job Application automation/linkedin.py
+++ Day49 - Job Application automation/linkedin.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-EMAIL = 'jdfhbvhfdbvhfo89@gmail.com'
+EMAIL = 'jdfhbvhfdbvhfo9@gmail.com'
 PASSWORD = 'FakePAsswrd'
 PHONE = '2061231234'
 
@@ -13,7 +13,7 @@ s = Service('/Users/jencastro/chromedriver')
 driver = webdriver.Chrome(service=s)
 url = 'https://www.linkedin.com/jobs/search/?f_AL=true&geoId=104116203&keywords=security%20engineer&location=Seattle%2C%20Washington%2C%20United%20States'
 driver.get(url)
-
+'#react-root > div > div > div > main > div > div > div > div.css-1dbjc4n.r-6koalj.r-16y2uox > div.css-1dbjc4n.r-16y2uox.r-1jgb5lz.r-13qz1uu > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input'
 
 sign_in = driver.find_element(By.LINK_TEXT, "Sign in")
 sign_in.click()
--- Day51 - automatetweet/speedtest.py
+++ Day51 - automatetweet/speedtest.py
@@ -10,12 +10,12 @@ import time
 
 DOWNLOAD = 300
 UPLOAD = 10
-PATH = '/Users/jencastrochromedriver'
-TWITTER_EMAIL = 'Jvhbdfvbfif@gmail.com'
-TWITTER_PASSWORD = "Fakepassword!"
+PATH = '/Users/jensmac/chromedriver'
+TWITTER_EMAIL = 'jngkngekr@gmail.com'
+TWITTER_PASSWORD = "Fakepwsdfd3!"
 URL = 'https://www.speedtest.net/'
 URL2 = 'https://twitter.com/login'
-S = Service('/Users/jencastro/chromedriver')
+S = Service(PATH)
 
 class InternetSpeedTwitterBot:
 
@@ -43,25 +43,34 @@ class InternetSpeedTwitterBot:
         self.driver.get(URL2)
         time.sleep(2)
 
-        username = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[5]/label/div/div[2]/div/input')
+        username = self.driver.find_element(By.NAME, 'text')
         username.send_keys(TWITTER_EMAIL)
+        time.sleep(2)
 
         next_btn = self.driver.find_element(By.XPATH, '//span[text()="Next"]')
         next_btn.click()
         time.sleep(3)
 
-        pw = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        username2 = self.driver.find_element(By.NAME, 'text')
+        username2.send_keys("offsecjen")
+        time.sleep(2)
+
+        next_btn = self.driver.find_element(By.XPATH, '//span[text()="Next"]')
+        next_btn.click()
+        time.sleep(3)
+
+        pw = self.driver.find_element(By.NAME, 'password')
         pw.send_keys(TWITTER_PASSWORD)
         pw.send_keys(Keys.ENTER)
-        time.sleep(15)
+        time.sleep(3)
 
         tweet = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
-        tweet.send_keys(f"My current Xfinity download speed: {self.down}, upload speed: {self.up}")
+        tweet.send_keys(f"My current @Xfinity download speed: {self.down}, upload speed: {self.up}. Promised speed 300 down, 10 up. I am Jen's bot.")
         time.sleep(3)
-        tweet_btn = self.driver.find_element(By.XPATH, '//button[text()="Tweet"]')
+
+        tweet_btn = self.driver.find_element(By.XPATH, '//span[text()="Tweet"]')
         tweet_btn.click()
 
-        self.driver.quit()
 
 bot = InternetSpeedTwitterBot(PATH)
 bot.get_internet_speed()
